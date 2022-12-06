f315926da9e03f3f3e93512b354f579c52397460
JAEMOON <jaemoonzzang@naver.com>
2022-12-06 11:32:15 +0900

    요청 -> 요청목록으로 데이터 적재 및 이동

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,49 +1,21 @@
 import axios from "axios";
 
-
 export function callAuthApi(api, methodType, params, token) {
   return new Promise((resolve, reject) => {
     let result = {};
     let data = {
-        method: methodType,
-        headers: { 
-            "content-type": "application/json",
-            "Authorization": token
-        }
+      method: methodType,
+      headers: {
+        "content-type": "application/json",
+        Authorization: token,
+      },
     };
     if (methodType === "POST") {
       data.body = JSON.stringify(params);
     }
 
     try {
-        fetch(api,data)
-        .then((res) => res.json())
-        .then((json) => {
-          if (json.status !== 200) {
-            result = json;
-          } 
-            resolve(json);
-        });
-    } catch (error) {
-        result = error;
-        console.error('error', error);
-        reject(error);
-    } finally {
-        console.log('api call end');
-    }
-    return result;
-  })
-}
-
-export function callApi(api,params) {
-  return new Promise((resolve, reject) => {
-    let result = {};
-    try {
-        fetch(api, {
-            method: "post",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(params)
-        })
+      fetch(api, data)
         .then((res) => res.json())
         .then((json) => {
           if (json.status !== 200) {
@@ -52,72 +24,125 @@ export function callApi(api,params) {
           resolve(json);
         });
     } catch (error) {
-        result = error;
-        console.error('error', error);
-        reject(error);
+      result = error;
+      console.error("error", error);
+      reject(error);
     } finally {
-        console.log('api call end');
+      console.log("api call end");
     }
     return result;
   });
 }
 
-export function callQuery(api,params) {
-    //   const result = [];
-    return new Promise((resolve, reject) => {
-      let result = [];
-      try {
-        fetch(api, {
-          method: "post",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({params})
-        })
-          .then((res) => res.json())
-          .then((json) => {
+export function callApi(api, params) {
+  return new Promise((resolve, reject) => {
+    let result = {};
+    try {
+      fetch(api, {
+        method: "post",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(params),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          if (json.status !== 200) {
             result = json;
-            resolve(result);
-          });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
+          }
+          resolve(json);
+        });
+    } catch (error) {
+      result = error;
+      console.error("error", error);
+      reject(error);
+    } finally {
+      console.log("api call end");
+    }
+    return result;
+  });
+}
+
+export function callQuery(api, params) {
+  //   const result = [];
+  return new Promise((resolve, reject) => {
+    let result = [];
+    try {
+      fetch(api, {
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ params }),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          result = json;
+          resolve(result);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
 
 /** 로그인 여부 체크  */
-export function isLogin () {
+export function isLogin() {
   return !!localStorage.getItem("login-token");
 }
 
 /** 오늘날짜 가져오기 */
 export function getToday() {
-    let today = new Date();   
+  let today = new Date();
 
-    let year = today.getFullYear(); // 년도
-    let month = today.getMonth() + 1;  // 월
-    let date = today.getDate();  // 날짜
-    month = (String(month) || '0').length === 1? `0${month}` : month; 
-    date = (String(date) || '0').length === 1? `0${date}` : date;
-    return (year + '-' + month + '-' + date);
+  let year = today.getFullYear(); // 년도
+  let month = today.getMonth() + 1; // 월
+  let date = today.getDate(); // 날짜
+  month = (String(month) || "0").length === 1 ? `0${month}` : month;
+  date = (String(date) || "0").length === 1 ? `0${date}` : date;
+  return year + "-" + month + "-" + date;
 }
 
 export function makeDate(targetDate) {
+  let year = targetDate.getFullYear(); // 년도
+  let month = targetDate.getMonth() + 1; // 월
+  let date = targetDate.getDate(); // 날짜
+  let hours = targetDate.getHours();
+  let minutes = targetDate.getHours();
 
-    let year = targetDate.getFullYear(); // 년도
-    let month = targetDate.getMonth() + 1;  // 월
-    let date = targetDate.getDate();  // 날짜
-    let hours = targetDate.getHours();
-    let minutes = targetDate.getHours();
+  month = (String(month) || "0").length === 1 ? `0${month}` : month;
+  date = (String(date) || "0").length === 1 ? `0${date}` : date;
+  hours = (String(hours) || "0").length === 1 ? `0${hours}` : hours;
+  minutes = (String(minutes) || "0").length === 1 ? `0${minutes}` : minutes;
 
-    month = (String(month) || '0').length === 1? `0${month}` : month; 
-    date = (String(date) || '0').length === 1? `0${date}` : date;
-    hours= (String(hours) || '0').length === 1? `0${hours}` : hours;
-    minutes= (String(minutes) || '0').length === 1? `0${minutes}` : minutes;
+  return year + "." + month + "." + date + ". " + hours + "시" + minutes + "분";
+}
 
-    return (
-      year + "." + month + "."  + date + ". " + hours + "시" + minutes + "분" 
-    )
+export function makeWorkRequestListColumn() {
+  const result = [
+    { field: "id", headerName: "요청ID", flex: 1 },
+    { field: "title", headerName: "제목", flex: 1 },
+    { field: "requestedBy", headerName: "작성자", flex: 1 },
+    { field: "date", headerName: "작성일시", flex: 1 },
+    { field: "totList", headerName: "", flex: 1, hide: true },
+  ];
+  return result;
+}
 
-
+export function makeWorkRequestListRows(workRequestList) {
+  return Array.isArray(workRequestList)
+    ? workRequestList.map((v, i) => {
+        return {
+          id: v["id"],
+          title: v["title"] || "",
+          requestedBy: v["requestedBy"],
+          date: makeDate(new Date(v["createdAt"])),
+          totList: v,
+        };
+      })
+    : {
+        id: workRequestList["id"],
+        title: workRequestList["title"] || "",
+        requestedBy: workRequestList["requestedBy"],
+        date: makeDate(new Date(workRequestList["createdAt"])),
+        totList: workRequestList,
+      };
 }
